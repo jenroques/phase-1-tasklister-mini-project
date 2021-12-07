0d32f28a8c074a36a41224ec74535af234dfeb8a
Jennifer Roques <jenniferroques@gmail.com>
2021-12-07 15:35:59 -0600

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const description = document.getElementById("new-task-description")
+    buildToDo(description)
+  })
+})
+
+function buildToDo(toDo) {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'X'
+  p.innerHTML = `${toDo.value} `
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#tasks').appendChild(p)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
